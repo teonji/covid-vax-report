6acb2d53b16bfbcfd7f40c79ef83fd6f5b92228d
matteo.tritto <matteo.tritto@weroad.it>
2021-01-11 20:18:11 +0100

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -92,7 +92,7 @@
             <a href="https://github.com/teonji/covid-vax-report"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
         </div>
         <div class="text-lg flex justify-end">
-          <span class="mr-2">Api by <a href="https://www.linkedin.com/in/a-marchetti">marquez</a></span>
+          <span class="mr-2">API by <a href="https://www.linkedin.com/in/a-marchetti">marquez</a></span>
           <a href="https://github.com/andreamarchetti/covid19-vaccini"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
         </div>
       </div>
@@ -162,70 +162,74 @@ const dataGet = [
     slug: 'somministrazioni-vaccini-latest',
     resolver: jsonData => {
       let json = _.get(jsonData, 'data[0].jsonData')
-      return _.mapValues(_.groupBy(json.data, 'area'), v => ({
-          doctors: _.sum(_.map(v, 'categoria_operatori_sanitari_sociosanitari')),
-          rsaGuests: _.sum(_.map(v, 'categoria_ospiti_rsa')),
-          others: _.sum(_.map(v, 'categoria_personale_non_sanitario')),
-          last: moment(_.last(_.map(v, 'data_somministrazione'))),
-          male: _.sum(_.map(v, 'sesso_maschile')),
-          female: _.sum(_.map(v, 'sesso_femminile')),
-          total: _.sum(_.map(v, 'sesso_maschile')) + _.sum(_.map(v, 'sesso_femminile')),
-          ageGroups: _.mapValues(_.groupBy(v, 'fascia_anagrafica'), vv => {
-            return {
-              doctors: _.sum(_.map(vv, 'categoria_operatori_sanitari_sociosanitari')),
-              rsaGuests: _.sum(_.map(vv, 'categoria_ospiti_rsa')),
-              others: _.sum(_.map(vv, 'categoria_personale_non_sanitario')),
-              last: moment(_.last(_.map(vv, 'data_somministrazione'))),
-              male: _.sum(_.map(vv, 'sesso_maschile')),
-              female: _.sum(_.map(vv, 'sesso_femminile')),
-              total: _.sum(_.map(vv, 'sesso_maschile')) + _.sum(_.map(vv, 'sesso_femminile')),
-              list: _.map(vv, val => ({
-                doctors: val.categoria_operatori_sanitari_sociosanitari,
-                rsaGuests: val.categoria_ospiti_rsa,
-                others: val.categoria_personale_non_sanitario,
-                date: moment(val.data_somministrazione),
-                male: val.sesso_maschile,
-                female: val.sesso_femminile,
-                total: val.sesso_maschile + val.sesso_femminile,
-              })),
-            }
-          }),
-          list: _.map(v, val => ({
-            doctors: val.categoria_operatori_sanitari_sociosanitari,
-            rsaGuests: val.categoria_ospiti_rsa,
-            others: val.categoria_personale_non_sanitario,
-            date: moment(val.data_somministrazione),
-            male: val.sesso_maschile,
-            female: val.sesso_femminile,
-          })),
-      }))
-    },
-  },
-  {
-    name: 'summaryAdministered',
-    slug: 'somministrazioni-vaccini-summary-latest',
-    resolver: jsonData => {
-      let json = _.get(jsonData, 'data[0].jsonData')
-      return _.mapValues(_.groupBy(json.data, 'area'), v => ({
+      return _.mapValues(_.groupBy(json.data, 'area'), (v, k) => ({
+        area: k,
         doctors: _.sum(_.map(v, 'categoria_operatori_sanitari_sociosanitari')),
         rsaGuests: _.sum(_.map(v, 'categoria_ospiti_rsa')),
         others: _.sum(_.map(v, 'categoria_personale_non_sanitario')),
         last: moment(_.last(_.map(v, 'data_somministrazione'))),
         male: _.sum(_.map(v, 'sesso_maschile')),
         female: _.sum(_.map(v, 'sesso_femminile')),
-        total: _.sum(_.map(v, 'totale')),
+        total: _.sum(_.map(v, 'sesso_maschile')) + _.sum(_.map(v, 'sesso_femminile')),
+        ageGroups: _.mapValues(_.groupBy(v, 'fascia_anagrafica'), vv => {
+          return {
+            area: k,
+            doctors: _.sum(_.map(vv, 'categoria_operatori_sanitari_sociosanitari')),
+            rsaGuests: _.sum(_.map(vv, 'categoria_ospiti_rsa')),
+            others: _.sum(_.map(vv, 'categoria_personale_non_sanitario')),
+            last: moment(_.last(_.map(vv, 'data_somministrazione'))),
+            male: _.sum(_.map(vv, 'sesso_maschile')),
+            female: _.sum(_.map(vv, 'sesso_femminile')),
+            total: _.sum(_.map(vv, 'sesso_maschile')) + _.sum(_.map(vv, 'sesso_femminile')),
+            list: _.map(vv, val => ({
+              area: k,
+              doctors: val.categoria_operatori_sanitari_sociosanitari,
+              rsaGuests: val.categoria_ospiti_rsa,
+              others: val.categoria_personale_non_sanitario,
+              date: moment(val.data_somministrazione),
+              male: val.sesso_maschile,
+              female: val.sesso_femminile,
+              total: val.sesso_maschile + val.sesso_femminile,
+            })),
+          }
+        }),
         list: _.map(v, val => ({
+          area: k,
           doctors: val.categoria_operatori_sanitari_sociosanitari,
           rsaGuests: val.categoria_ospiti_rsa,
           others: val.categoria_personale_non_sanitario,
           date: moment(val.data_somministrazione),
           male: val.sesso_maschile,
           female: val.sesso_femminile,
-          total: val.totale,
         })),
       }))
     },
   },
+  // {
+  //   name: 'summaryAdministered',
+  //   slug: 'somministrazioni-vaccini-summary-latest',
+  //   resolver: jsonData => {
+  //     let json = _.get(jsonData, 'data[0].jsonData')
+  //     return _.mapValues(_.groupBy(json.data, 'area'), v => ({
+  //       doctors: _.sum(_.map(v, 'categoria_operatori_sanitari_sociosanitari')),
+  //       rsaGuests: _.sum(_.map(v, 'categoria_ospiti_rsa')),
+  //       others: _.sum(_.map(v, 'categoria_personale_non_sanitario')),
+  //       last: moment(_.last(_.map(v, 'data_somministrazione'))),
+  //       male: _.sum(_.map(v, 'sesso_maschile')),
+  //       female: _.sum(_.map(v, 'sesso_femminile')),
+  //       total: _.sum(_.map(v, 'totale')),
+  //       list: _.map(v, val => ({
+  //         doctors: val.categoria_operatori_sanitari_sociosanitari,
+  //         rsaGuests: val.categoria_ospiti_rsa,
+  //         others: val.categoria_personale_non_sanitario,
+  //         date: moment(val.data_somministrazione),
+  //         male: val.sesso_maschile,
+  //         female: val.sesso_femminile,
+  //         total: val.totale,
+  //       })),
+  //     }))
+  //   },
+  // },
   {
     name: 'administrationPoints',
     slug: 'punti-somministrazione-latest',
@@ -357,6 +361,35 @@ export default {
   computed: {
     administrations () {
       return _.sum(_.map(this.administeredByRegion, 'total'))
+    },
+    administrationsDays () {
+      return _.chain(this.administeredByRegion)
+        .map('list')
+        .flatten()
+        .groupBy(f => f.date.format('YYYY-MM-DD'))
+        .mapValues(v => {
+          const list = _.mapValues(_.groupBy(v, 'area'), vv => {
+            const doctors = _.sum(_.map(vv, 'doctors'))
+            const others = _.sum(_.map(vv, 'others'))
+            const rsaGuests = _.sum(_.map(vv, 'rsaGuests'))
+            const female = _.sum(_.map(vv, 'female'))
+            const male = _.sum(_.map(vv, 'male'))
+            const total = female + male
+            return {
+              doctors,
+              others,
+              rsaGuests,
+              female,
+              male,
+              total
+            }
+          })
+          return {
+            total: _.sum(_.map(list, 'total')),
+            list
+          }
+        })
+        .value()
     },
     deliveriesVaccinesTotal () {
       return _.sum(_.map(this.deliveriesVaccines, 'doses'))
